f71d9b6b39abf2fb0a48b9a612276bc4dfb99b27
Jacob <jakeywaldrip@gmail.com>
2024-06-30 01:24:56 -0500

    Added rust project

---
+++ target/debug/deps/kungfu_os-42b1f646900e183e.d
@@ -0,0 +1,5 @@
+C:\Users\jcwal\Development\kungfu_os\target\debug\deps\libkungfu_os-42b1f646900e183e.rmeta: src\main.rs
+
+C:\Users\jcwal\Development\kungfu_os\target\debug\deps\kungfu_os-42b1f646900e183e.d: src\main.rs
+
+src\main.rs:
