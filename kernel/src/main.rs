ed0cdb360cfbb75983bafa04f3e8d2b374e8ef6e
Jacob <jakeywaldrip@gmail.com>
2024-07-03 02:30:06 -0500

    Launching kernel shortcut and writing pixels to screen

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,7 +3,12 @@
 
 bootloader_api::entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+  if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+    for byte in framebuffer.buffer_mut() {
+      *byte = 0x90;
+    }
+  }
   loop {}
 }
 
